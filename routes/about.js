ed8a525ff7b6db3c49d65e85735d84fa225cb577
eyun1988 <eyun1@mail.sfsu.edu>
2021-08-28 15:57:58 -0700

    profile page done

--- routes/about.js
+++ routes/about.js
@@ -1,48 +1,74 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const options = {
+    "Adele": 0, 
+    "Jeff": 1, 
+    "Kris": 2, 
+    "Jeffrey": 3,
+    "Eddy": 4,
+    "Jose": 5 
+};
+
+const person = [
+    {
+        first_name:"Adele",
+        last_name: "Wu",
+        image: "Smiley_Face.JPG",
+        github: "adele-wu",
+        information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie felis dui, suscipit commodo ligula egestas ac. In sit amet magna vel est ullamcorper commodo sit amet non nulla. Nunc vulputate orci mauris, a aliquam ligula commodo et. Suspendisse potenti. Vestibulum in condimentum felis. Sed semper leo in neque elementum fringilla. Maecenas elementum ornare ipsum placerat mattis. Maecenas non porta arcu, eget suscipit felis. Aenean nec eros quis nisi pretium viverra. Quisque a rhoncus quam. "
+    },
+    {
+        first_name : "Jeff",
+        last_name:  "Fulmer Gardner",
+        image: "Smiley_Face.JPG",
+        github: "JeffreyFG",
+        information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie felis dui, suscipit commodo ligula egestas ac. In sit amet magna vel est ullamcorper commodo sit amet non nulla. Nunc vulputate orci mauris, a aliquam ligula commodo et. Suspendisse potenti. Vestibulum in condimentum felis. Sed semper leo in neque elementum fringilla. Maecenas elementum ornare ipsum placerat mattis. Maecenas non porta arcu, eget suscipit felis. Aenean nec eros quis nisi pretium viverra. Quisque a rhoncus quam. "
+    },
+    {
+        first_name : "Kris",
+        last_name: "Byington",
+        image: "Smiley_Face.JPG",
+        github: "krisbyington",
+        information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie felis dui, suscipit commodo ligula egestas ac. In sit amet magna vel est ullamcorper commodo sit amet non nulla. Nunc vulputate orci mauris, a aliquam ligula commodo et. Suspendisse potenti. Vestibulum in condimentum felis. Sed semper leo in neque elementum fringilla. Maecenas elementum ornare ipsum placerat mattis. Maecenas non porta arcu, eget suscipit felis. Aenean nec eros quis nisi pretium viverra. Quisque a rhoncus quam. "
+    },
+    {
+        first_name : "Jeffrey",
+        last_name: "Friedrich",
+        image: "Smiley_Face.JPG",
+        github: "jeffreyfriedrich",
+        information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie felis dui, suscipit commodo ligula egestas ac. In sit amet magna vel est ullamcorper commodo sit amet non nulla. Nunc vulputate orci mauris, a aliquam ligula commodo et. Suspendisse potenti. Vestibulum in condimentum felis. Sed semper leo in neque elementum fringilla. Maecenas elementum ornare ipsum placerat mattis. Maecenas non porta arcu, eget suscipit felis. Aenean nec eros quis nisi pretium viverra. Quisque a rhoncus quam. "
+    },
+    {
+        first_name : "Eddy",
+        last_name : "Yun",
+        image: "Smiley_Face.JPG",
+        github: "eyun1988",
+        information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie felis dui, suscipit commodo ligula egestas ac. In sit amet magna vel est ullamcorper commodo sit amet non nulla. Nunc vulputate orci mauris, a aliquam ligula commodo et. Suspendisse potenti. Vestibulum in condimentum felis. Sed semper leo in neque elementum fringilla. Maecenas elementum ornare ipsum placerat mattis. Maecenas non porta arcu, eget suscipit felis. Aenean nec eros quis nisi pretium viverra. Quisque a rhoncus quam. "
+    },
+    {
+        first_name : "Jose",
+        last_name : "Quintero",
+        image: "Smiley_Face.JPG",
+        github: "",
+        information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie felis dui, suscipit commodo ligula egestas ac. In sit amet magna vel est ullamcorper commodo sit amet non nulla. Nunc vulputate orci mauris, a aliquam ligula commodo et. Suspendisse potenti. Vestibulum in condimentum felis. Sed semper leo in neque elementum fringilla. Maecenas elementum ornare ipsum placerat mattis. Maecenas non porta arcu, eget suscipit felis. Aenean nec eros quis nisi pretium viverra. Quisque a rhoncus quam. "
+    }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    let person = [
-        {
-            name : "Adele Wu",
-            image: "Smiley_Face.JPG",
-            github: "adele-wu",
-            information: ""
-        },
-        {
-            name : "Jeffrey Fulmer Gardner",
-            image: "Smiley_Face.JPG",
-            github: "",
-            information: ""
-        },
-        {
-            name : "Kris Byington",
-            image: "Smiley_Face.JPG",
-            github: "krisbyington",
-            information: ""
-        },
-        {
-            name : "Jeffrey Friedrich",
-            image: "Smiley_Face.JPG",
-            github: "jeffreyfriedrich",
-            information: ""
-        },
-        {
-            name : "Eddy Yun",
-            image: "Smiley_Face.JPG",
-            github: "eyun1988",
-            information: ""
-        },
-        {
-            name : "Jose Quintero",
-            image: "Smiley_Face.JPG",
-            github: "",
-            information: ""
-        }
-    ];
+    // TODO: Replace static array with MySql query.
     res.locals.logged = true;
     res.render('about_us', { title: "About Page", person }); // can only pass an array
+});
+
+// route to page based on client choice
+router.get('/:person', function(req, res, next) {
+    const first_name = req.params.person;
+    console.log(first_name);
+    if(first_name in options) 
+        return res.render('about_me', {title: person[options[first_name]].first_name, person: person[options[first_name]]});
+    return res.redirect("/about")
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home Page' });
 });
 
+router.get('/registration', function(req, res, next) {
+  res.render('registration', { title: 'Registration Page' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login Page' });
+});
+
 module.exports = router;
